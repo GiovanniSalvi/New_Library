69265ce8d01c65041cceefba9cf25982368470fe
GiovanniSalvi <salvigiovanni4@gmail.com>
2020-12-27 01:11:14 +0000

    base template created

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-from flask import Flask
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -13,16 +12,16 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("roBJgSPzhd")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
 
 @app.route("/")
 @app.route("/get_tasks")
-def get_tasks():
-    tasks = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks=tasks)
+def get_BooksData():
+    tasks = mongo.db.BooksData.find()
+    return render_template("index.html", tasks=tasks)
 
 
 if __name__ == "__main__":
