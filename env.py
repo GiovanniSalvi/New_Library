cebbdaff6e71cd5f251664ab24be3d9d27864409
Giovanni Salvi <61980577+GiovanniSalvi@users.noreply.github.com>
2021-01-15 21:57:05 +0100

    Update env.py

--- env.py
+++ env.py
@@ -2,6 +2,6 @@ import os
 
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORTA", "5000")
-os.environ.setdefault("SECRET_KEY", "roBJgSPzhd")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://root:wjcKEtzGGfkbIqLB@datacentricdevelopmentm.h4cwy.mongodb.net/DataCentricProject?retryWrites=true&w=majority")
+os.environ.setdefault("SECRET_KEY", "RQMcarCGiErfOqyD")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:FGSOgtEbvBaRVVnN@datacentricdevelopmentm.h4cwy.mongodb.net/DataCentricProject?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "DataCentricProject")
