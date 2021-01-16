c59e22da7c77453eeb8bdafc3ca21e1f618429b4
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-01-16 11:11:27 +0000

    change 'PORT' name to fix heroku deployment issue

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ def register():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORTA")),
+            port=int(os.environ.get("PORT")),
             debug=True)
 
 
