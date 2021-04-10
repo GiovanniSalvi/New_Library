2941bd1dea459b077d0d66b40f14898bfa3f51a5
Giovanni Salvi <61980577+GiovanniSalvi@users.noreply.github.com>
2021-04-10 14:13:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -221,4 +221,4 @@ if __name__ == "__main__":
         host=os.environ.get(
             "IP"), port=int(
                 os.environ.get(
-                    "PORT")), debug=False)
+                    "PORT")), debug=True)
